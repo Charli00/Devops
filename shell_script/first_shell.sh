4911fd09090bcb4d40f731da679d28fc7ddb665a
peniel charles p <98569512+Charli00@users.noreply.github.com>
2025-05-21 05:06:04 +0000

    Changes made

--- shell_script/first_shell.sh
+++ shell_script/first_shell.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -x            # print each command (debug)
-set -e            # exit immediately if any simple command fails
-set -o pipefail   # make a pipeline fail if *any* command in it fails
-
+# set -e            # exit immediately if any simple command fails
+# set -0 pipefail   # make a pipeline fail if *any* command in it fails
+set -euo pipefail
 # Without `pipefail`, the next line would hide the error from `wdhfihadufh`
 # because `echo` (the last command in the pipeline) succeeds.
 wdhfihadufh | echo
 
-echo "hello world"
+echo "hello world" | asdhuags
+
+df -kh
