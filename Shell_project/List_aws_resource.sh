5a999b01a147e848cd9380e5fa455e1f33cc4356
peniel charles p <98569512+Charli00@users.noreply.github.com>
2025-05-22 10:27:54 +0000

    unwanted deleted

---
+++ Shell_project/List_aws_resource.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###################
+
+#Author: Charles
+#DATE: 22-05-25
+#List all the resources in AWS
+
+#List the s3
+aws s3 ls
+
+#List the ec2 instances architectures 
+#jq for json
+#yq for yaml
+
+aws ec2 describe-instances > aws.log
+# aws ec2 describe-instances | jq '.Reservations[].Instances[].Architecture'
+
+#List the IAM users
+aws iam list-users
+
+ls -lrht ;ps -ef > new.log
+
+# rm new.log  
+# rm aws.log
+
+
+
