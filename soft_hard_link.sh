38476fac0c84d8b655ba1930fe64c5c72071bf00
peniel charles p <98569512+Charli00@users.noreply.github.com>
2025-05-22 05:02:25 +0000

    soft and hard link

---
+++ soft_hard_link.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+########################Author####################
+
+#Here are learning about the shell script soft and hard link
+#We wrote the few example on below .
+
+#############################################
+
+#create the new file
+
+echo "SOFT AND HARD LINK" > original.txt
+
+#create the soft link
+ln -s original.txt soft.txt
+
+#create the hard link
+ln original.txt hard.txt
+
+#remove the original text file
+# rm original.txt
+
+cat soft.txt #It will not display after the remove of the original file
+cat hard.txt
+
+
